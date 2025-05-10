32989d88c6ddc96654b2bfbda36182bcf7b3bcce
alacritor <alacrity.dh@gmail.com>
2025-05-10 13:54:46 -0700

    Terra-Router-Nat-Push

--- GCP/4-router.tf
+++ GCP/4-router.tf
@@ -5,10 +5,22 @@ resource "google_compute_router" "aperture-router-oregon" {
   network = google_compute_network.aperture-vpc.id
 }
 
+resource "google_compute_router" "aperture-router-oregon-prod" {
+  name    = "aperture-router-oregon-prod"
+  region  = "us-west1"
+  network = google_compute_network.aperture-vpc-prod.id
+}
+
 resource "google_compute_router" "aperture-router-tokyo" {
   name    = "aperture-router-tokyo"
   region  = "asia-northeast1"
   network = google_compute_network.aperture-vpc.id
+}
+
+resource "google_compute_router" "aperture-router-tokyo-prod" {
+  name    = "aperture-router-tokyo-prod"
+  region  = "asia-northeast1"
+  network = google_compute_network.aperture-vpc-prod.id
 }
 
 resource "google_compute_router" "aperture-router-london" {
@@ -17,8 +29,21 @@ resource "google_compute_router" "aperture-router-london" {
   network = google_compute_network.aperture-vpc.id
 }
 
+resource "google_compute_router" "aperture-router-london-prod" {
+  name    = "aperture-router-london-prod"
+  region  = "europe-west2"
+  network = google_compute_network.aperture-vpc-prod.id
+}
+
 resource "google_compute_router" "aperture-router-paris" {
   name    = "aperture-router-paris"
   region  = "europe-west9"
   network = google_compute_network.aperture-vpc.id
 }
+
+resource "google_compute_router" "aperture-router-paris-prod" {
+  name    = "aperture-router-paris-prod"
+  region  = "europe-west9"
+  network = google_compute_network.aperture-vpc-prod.id
+}
+
--- GCP/5-nat.tf
+++ GCP/5-nat.tf
@@ -53,3 +53,31 @@ resource "google_compute_address" "aperture-nat-tokyo" {
 
   # depends_on = [google_project_service.compute]
 }
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_router_nat
+resource "google_compute_router_nat" "aperture-nat-london" {
+  name   = "aperture-nat-london"
+  router = google_compute_router.aperture-router-london.name
+  region = "europe-west2"
+
+
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  nat_ip_allocate_option             = "MANUAL_ONLY"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.london1.id
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  nat_ips = [google_compute_address.aperture-nat-london.self_link]
+}
+
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_address
+resource "google_compute_address" "aperture-nat-london" {
+  name         = "aperture-nat-london"
+  address_type = "EXTERNAL"
+  network_tier = "PREMIUM"
+  region       = "europe-west2"
+
+  # depends_on = [google_project_service.compute]
+}
