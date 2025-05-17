7fa6b09f3a484ad7ac78962ea259aacaa5d5447d
alacritor <alacrity.dh@gmail.com>
2025-05-17 13:36:52 -0700

    Terra-Firewall-Fix

--- GCP/6-firewall.tf
+++ GCP/6-firewall.tf
@@ -28,4 +28,36 @@ resource "google_compute_firewall" "allow-aperture-web-traffic" {
 
   source_ranges = ["0.0.0.0/0"]
 
-} 
+}
+
+
+
+
+# For Aperture-VPC-Prod
+resource "google_compute_firewall" "aperture-firewall-prod" {
+  name      = "aperture-firewall-prod"
+  network   = google_compute_network.aperture-vpc-prod.name
+  direction = "INGRESS"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow-aperture-web-traffic-prod" {
+  name    = "allow-aperture-web-traffic-prod"
+  network = google_compute_network.aperture-vpc-prod.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+
+}
+
+
